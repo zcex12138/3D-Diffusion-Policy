e88785cefa5bbe63eae67cac82bf2c6c8dbe2ba4
YangYi2004 <1713282820@qq.com>
2025-11-05 14:19:17 -0500

    add cameras to the dphand

---
+++ README.md
@@ -0,0 +1,19 @@
+# 添加相机
+在 `third_party/dphand/dphand_env/assets/panda_dphand.xml` 中添加相机
+
+# 渲染深度图
+以 `diffusion_policy_3d/env/dphand/test_env.py` 为例
+
+将 62 行修改为
+```python
+# depth = obs['depth'].copy()
+_, depth = env.unwrapped._viewer.render_segment_depth(env.unwrapped.cam_ids["front"])
+```
+可以获取对应名称相机的深度图
+
+# 关于环境的说明
+- `MujocoEnv`: Gym中定义的Mujoco环境基类，结合了一些mujoco的基础api,实现了一些基础功能
+- `MujocoGymEnv`: 继承自 `MujocoEnv`，主要添加了渲染功能，参考 `mujoco_render.py`
+- `BaseEnv`: 继承自 `MujocoGymEnv`，主要有两点变化：1.支持从config文件中读取环境参数；2.支持运行多个物理步 # 可以考虑把这个环境和`DphandPandaEnv`合并，因为`dphand_env`已经弃用（这个环境是为之前没有franka panda的时候写的）
+- `DphandPandaEnv`: 继承自 `BaseEnv`，主要添加了灵巧手和franka panda相关的属性，包括franka panda的阻抗控制器
+- `PickAndPlaceEnv`: 继承自 `DphandPandaEnv`，面向任务级别的环境，例如pick and place
---
+++ third_party/dphand/visualize_model.py
@@ -0,0 +1,25 @@
+import mujoco
+import mujoco.viewer
+from pynput import keyboard
+
+model = mujoco.MjModel.from_xml_path("third_party/dphand/dphand_env/assets/panda_dphand_with_tip_camera.xml")  # 改成你的xml路径
+data = mujoco.MjData(model)
+
+def _start_escape_listener(viewer):
+    def on_press(key):
+        if key == keyboard.Key.esc:
+            viewer.close()
+            return False
+    listener = keyboard.Listener(on_press=on_press)
+    listener.start()
+    return listener
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    esc_listener = _start_escape_listener(viewer)
+    try:
+        while viewer.is_running():
+            mujoco.mj_step(model, data)
+            viewer.sync()
+    finally:
+        if esc_listener is not None:
+            esc_listener.stop()
